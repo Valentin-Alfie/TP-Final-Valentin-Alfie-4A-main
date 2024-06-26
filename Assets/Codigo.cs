5e03b30b7ad5c50a055e2f4228d8ae7aeed596ee
Valentin Alfie <48178972@est.ort.edu.ar>
2024-06-26 04:01:05 -0300

    TP Domiciliario Unity

--- Assets/Codigo.cs
+++ Assets/Codigo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Codigo : MonoBehaviour
 {
@@ -8,7 +9,14 @@ public class Codigo : MonoBehaviour
     public int minX;
     public int maxX;
     int cantidad;
+    int SpawnsRestantes;
     int tipo;
+    public GameObject panelRespuesta;
+    public GameObject panelError;
+    public GameObject panelNotificaciones;
+    public InputField inputResultado;
+    public Text textoNotificaciones;
+
     Vector3 spawnPosicion;
     Quaternion spawnRotacion;
     // Start is called before the first frame update
@@ -21,22 +29,64 @@ public class Codigo : MonoBehaviour
         tipo = Random.Range(0, 26);
         Objetos[tipo].SetActive(true);
         cantidad = Random.Range(minX, maxX);
+        SpawnsRestantes = cantidad;
         spawnPosicion = Objetos[tipo].transform.position;
         spawnRotacion = Objetos[tipo].transform.rotation;
-        InvokeRepeating("SpawnObjeto", 0, 1);
+        InvokeRepeating("SpawnObjeto", 1, 1);
+        panelRespuesta.SetActive(true);
+        panelError.SetActive(false);
+        panelNotificaciones.SetActive(false);
     }
 
     // Update is called once per frame
     public void SpawnObjeto()
     {
-        if (cantidad > 0)
+        if (SpawnsRestantes > 0)
         {
             Instantiate(Objetos[tipo], spawnPosicion, spawnRotacion);
-            cantidad--;
+            SpawnsRestantes--;
         }
         else
         {
             CancelInvoke("SpawnObjeto");
         }
+    }
+
+    public void Respuesta()
+    {
+        string resultado = inputResultado.text;
+        if (resultado == "")
+        {
+            panelError.SetActive(true);
+            panelRespuesta.SetActive(false);
+        }
+        else
+        {
+            int resultadoInt = int.Parse(resultado);
+            if (resultadoInt == cantidad + 1)
+            {
+                panelNotificaciones.SetActive(true);
+                panelRespuesta.SetActive(false);
+                textoNotificaciones.text = "LA RESPUESTA ES CORRECTA!";
+            }
+            else
+            {
+                panelNotificaciones.SetActive(true);
+                panelRespuesta.SetActive(false);
+                textoNotificaciones.text = "LA RESPUESTA ES INCORRECTA!";
+            }
+        }
+    }
+
+    public void BotonError()
+    {
+        panelError.SetActive(false);
+        panelRespuesta.SetActive(true);
+    }
+
+    public void BotonNotificaciones()
+    {
+        panelNotificaciones.SetActive(false);
+        panelRespuesta.SetActive(true);
     }
 }
